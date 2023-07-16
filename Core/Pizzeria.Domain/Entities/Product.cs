5fa15e2782e989052ead8c2db3544277de14df64
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-16 13:02:04 +0300

    Сущность товара

---
+++ Core/Pizzeria.Domain/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using Pizzeria.Domain.Entities.Base;
+
+namespace Pizzeria.Domain.Entities;
+
+public class Product:Entity
+{
+    public decimal Price { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Order { get; set; }
+    public Section SectionId { get; set; }
+}
