3a310a630b316baff2e0033a63d2e72430be9eef
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-16 13:46:04 +0300

    Контекст базы данных

---
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/PizzeriaDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pizzeria.Infrastructure.Persistence.Configurations;
+
+namespace Pizzeria.Infrastructure.Persistence;
+
+public class PizzeriaDbContext : DbContext
+{
+   
+    public PizzeriaDbContext(DbContextOptions<PizzeriaDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfiguration(new OrderConfiguration());
+        builder.ApplyConfiguration(new OrderItemConfiguration());
+        builder.ApplyConfiguration(new ProductConfiguration());
+        builder.ApplyConfiguration(new SectionConfiguration());
+        base.OnModelCreating(builder);
+    }
+}
