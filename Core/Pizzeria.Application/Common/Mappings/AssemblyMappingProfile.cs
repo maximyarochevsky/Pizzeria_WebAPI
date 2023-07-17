29f3b7ec2d048d718cce53e8439f93a929765c66
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-17 12:33:49 +0300

    Automapper

--- Core/Pizzeria.Application/Common/Mappings/AssemblyMappingProfile.cs
+++ Core/Pizzeria.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pizzeria.Application.Common.Mappings
 {
-    internal class AssemblyMappingProfile
+    public class AssemblyMappingProfile
     {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            // Находим интерфейсы, которые зависят от IMapWith<>
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(i => i.IsGenericType &&
+                              i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+                .ToList();
+
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
     }
 }
