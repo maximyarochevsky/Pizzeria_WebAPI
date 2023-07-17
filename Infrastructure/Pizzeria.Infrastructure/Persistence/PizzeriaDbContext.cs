45d883a54846e3195f922665e96cf628fc7e25b1
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-17 11:27:13 +0300

    Таблицы контекста БД

--- Infrastructure/Pizzeria.Infrastructure/Persistence/PizzeriaDbContext.cs
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/PizzeriaDbContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pizzeria.Domain.Entities;
 using Pizzeria.Infrastructure.Persistence.Configurations;
 
 namespace Pizzeria.Infrastructure.Persistence;
 
 public class PizzeriaDbContext : DbContext
 {
-   
+    public DbSet<Section> Sections { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<Order>  Orders { get; set; }
+
     public PizzeriaDbContext(DbContextOptions<PizzeriaDbContext> options)
         : base(options) { }
+
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
