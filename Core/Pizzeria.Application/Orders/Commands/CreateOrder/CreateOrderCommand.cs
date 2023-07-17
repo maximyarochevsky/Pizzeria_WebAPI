e5ee8b7c0a058838f584f0babc0d576a77e079e3
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-17 12:34:25 +0300

    Запрос на создание заказа

---
+++ Core/Pizzeria.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Description { get; set; }
+    public ICollection<OrderItem> Items { get; set; }
+}
+
