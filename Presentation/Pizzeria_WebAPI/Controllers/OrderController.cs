8a4306d820a95f784d1cf42bb923a5e5e91e3862
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-19 14:46:23 +0300

    Контроллер азказов

---
+++ Presentation/Pizzeria_WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("create")]
+        public async Task<Guid> CreateOrder(Order order)
+        {
+            var command = order;
+        }
+    }
+}
