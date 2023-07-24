179ff6fad4e645ad7c6c33712a391275f9b60510
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-24 12:35:04 +0300

    Репозиторий с запросами для товаров

---
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using Microsoft.EntityFrameworkCore;
+using Pizzeria.Application.Interfaces.Persistence;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Infrastructure.Persistence.Repositories;
+
+public class ProductRepository : GenericRepository<Product>, IProductRepository
+{
+    public ProductRepository(PizzeriaDbContext dbContext):base(dbContext)
+    {
+    }
+
+    public Task<List<Product>> GetAllProducts()
+    {
+        return _dbContext.Products
+            .Include(p => p.Section)
+            .ToListAsync();
+    }
+
+    public Task<Product> GetProductById(Guid id)
+    {
+        return _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+    }
+
+    public Task<List<Product>> GetProductsBySection(Guid id)
+    {
+        return _dbContext.Products
+            .Include(p => p.Section)
+            .Where(p => p.Section.Id == id)
+            .ToListAsync();
+    }
+}
+
