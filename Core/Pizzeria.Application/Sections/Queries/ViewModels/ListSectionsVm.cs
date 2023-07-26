2e7e375ab04e39c60cfbb4ab916b288842d82b73
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-26 12:21:53 +0300

    Модель списка секций

---
+++ Core/Pizzeria.Application/Sections/Queries/ViewModels/ListSectionsVm.cs
@@ -0,0 +1,9 @@
+﻿using Pizzeria.Application.Common.Mappings;
+
+namespace Pizzeria.Application.Sections.Queries.ViewModels;
+
+public class ListSectionsVm
+{
+    public List<SectionVm> ListSections { get; set; }
+}
+
