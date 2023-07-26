44291cbd48ec26a7a05cd6991b13c44037334335
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-26 12:21:13 +0300

    Запрос на получение секции по Id

---
+++ Core/Pizzeria.Application/Sections/Queries/GetSectionById/GetSectionByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Pizzeria.Application.Sections.Queries.ViewModels;
+
+namespace Pizzeria.Application.Sections.Queries.GetSectionById;
+
+public class GetSectionByIdQuery: IRequest<SectionVm>
+{
+    public Guid Id { get; set; }
+}
+
