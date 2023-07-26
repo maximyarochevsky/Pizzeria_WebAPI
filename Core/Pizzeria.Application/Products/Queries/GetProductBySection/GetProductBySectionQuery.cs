4c03894fa90ef3c4214a7415bd74d328246b1e3e
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-26 11:46:23 +0300

    Запрос на получение всех товаров секции

---
+++ Core/Pizzeria.Application/Products/Queries/GetProductBySection/GetProductBySectionQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Pizzeria.Application.Products.Queries.ViewModels;
+
+namespace Pizzeria.Application.Products.Queries.GetProductBySection;
+
+public class GetProductBySectionQuery : IRequest<ListProductsVm>
+{
+    public Guid SectionId { get; set; }
+}
+
