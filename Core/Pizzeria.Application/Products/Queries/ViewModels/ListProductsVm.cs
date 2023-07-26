cf3fd0a94c6d865d0732a3b87582f9869791b065
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-26 11:47:06 +0300

    Модель списка товаров

---
+++ Core/Pizzeria.Application/Products/Queries/ViewModels/ListProductsVm.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Pizzeria.Application.Common.Mappings;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Products.Queries.ViewModels;
+
+public class ListProductsVm
+{
+    public List<ProductDetailsVm> ListProducts { get; set; }
+}
+
