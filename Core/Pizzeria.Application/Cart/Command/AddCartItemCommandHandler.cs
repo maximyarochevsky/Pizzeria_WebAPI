f7ce65b51aad117c13960311f2cd81db30b77e38
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-28 21:59:28 +0300

    Обработчик запроса на добавление товара в корзину

---
+++ Core/Pizzeria.Application/Cart/Command/AddCartItemCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Pizzeria.Application.Interfaces.Persistence;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Cart.Command;
+
+public class AddCartItemCommandHandler : IRequestHandler<AddCartItemCommand, CartItem>
+{
+    public IUnitOfWork _unitOfWork;
+
+    public AddCartItemCommandHandler(IUnitOfWork unitOfWork)
+        => _unitOfWork = unitOfWork;
+
+    public async Task<CartItem> Handle(AddCartItemCommand request, CancellationToken cancellationToken)
+    {
+        var cartItem = new CartItem()
+        {
+            ProductId = request.Product.Id,
+            Quantity = request.Quantity,
+        };
+
+        _unitOfWork.Cart.AddCartItem(cartItem);
+
+        return cartItem;
+    }
+}
+
