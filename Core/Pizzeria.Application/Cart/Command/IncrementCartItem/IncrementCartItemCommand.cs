92d805d90462191f0fac8ff088ed95ee44659059
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-31 16:02:33 +0300

    Запрос на инкремент товара в корзине

---
+++ Core/Pizzeria.Application/Cart/Command/IncrementCartItem/IncrementCartItemCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Cart.Command.IncrementCartItem;
+
+public class IncrementCartItemCommand : IRequest<bool>
+{
+    public Guid ProductId { get; set; }
+}
