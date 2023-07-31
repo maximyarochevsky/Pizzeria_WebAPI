b40eba298abe387f6547be674246a3f42ae4fee0
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-31 16:02:44 +0300

    Запрос на декремент товара в корзине

---
+++ Core/Pizzeria.Application/Cart/Command/IncrementCartItem/IncrementCartItemCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Pizzeria.Application.Interfaces.Persistence;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Cart.Command.IncrementCartItem;
+
+public class IncrementCartItemCommandHandler : IRequestHandler<IncrementCartItemCommand, bool>
+{
+    public IUnitOfWork _unitOfWork;
+
+    public IncrementCartItemCommandHandler(IUnitOfWork unitOfWork)
+        => _unitOfWork = unitOfWork;
+
+    public async Task<bool> Handle(IncrementCartItemCommand request, CancellationToken cancellationToken)
+    {
+        if(request.ProductId == null)
+            throw new ArgumentNullException(nameof(request.ProductId));
+
+        bool result = _unitOfWork.Cart.IncrementCartItem(request.ProductId);
+
+        return result;
+    }
+}
