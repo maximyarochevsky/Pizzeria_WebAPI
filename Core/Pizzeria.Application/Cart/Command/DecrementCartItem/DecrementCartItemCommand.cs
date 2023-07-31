ae4e7e6bd179170dc0ebfa34a7be33a2afc47e72
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-31 16:02:01 +0300

    Запрос на декремент товара в корзине

---
+++ Core/Pizzeria.Application/Cart/Command/DecrementCartItem/DecrementCartItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Pizzeria.Application.Cart.Command.DecrementCartItem;
+
+public class DecrementCartItemCommand : IRequest<bool>
+{
+    public Guid ProductId { get; set; }
+}
