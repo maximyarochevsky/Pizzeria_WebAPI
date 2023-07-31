61b449f714171fe5c6ed19f3a9db84b3f2e2f2c0
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-31 16:02:13 +0300

    Обработчик на декремент товара в корзине

---
+++ Core/Pizzeria.Application/Cart/Command/DecrementCartItem/DecrementCartItemCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Pizzeria.Application.Cart.Command.IncrementCartItem;
+using Pizzeria.Application.Interfaces.Persistence;
+
+namespace Pizzeria.Application.Cart.Command.DecrementCartItem;
+
+public class DecrementCartItemCommandHandler : IRequestHandler<DecrementCartItemCommand, bool>
+{
+    public IUnitOfWork _unitOfWork;
+
+    public DecrementCartItemCommandHandler(IUnitOfWork unitOfWork)
+        => _unitOfWork = unitOfWork;
+
+    public async Task<bool> Handle(DecrementCartItemCommand request, CancellationToken cancellationToken)
+    {
+        if (request.ProductId == null)
+            throw new ArgumentNullException(nameof(request.ProductId));
+
+        bool result = _unitOfWork.Cart.DecrementCartItem(request.ProductId);
+
+        return result;
+    }
+}
+
