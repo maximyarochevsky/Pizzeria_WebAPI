cb6bebcd625a40e09cf8466afc314dd05d17558e
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-31 16:03:07 +0300

    Обработчик запроса на удаление товара из корзины

---
+++ Core/Pizzeria.Application/Cart/Command/RemoveCartItem/RemoveCartItemCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Pizzeria.Application.Interfaces.Persistence;
+
+namespace Pizzeria.Application.Cart.Command.RemoveCartItem;
+
+public class RemoveCartItemCommandHandler : IRequestHandler<RemoveCartItemCommand, bool>
+{
+    public IUnitOfWork _unitOfWork;
+
+    public RemoveCartItemCommandHandler(IUnitOfWork unitOfWork)
+        => _unitOfWork = unitOfWork;
+
+    public async Task<bool> Handle(RemoveCartItemCommand request, CancellationToken cancellationToken)
+    {
+        if (request.ProductId == null)
+            throw new ArgumentNullException(nameof(request.ProductId));
+
+        bool result = _unitOfWork.Cart.RemoveCartItem(request.ProductId);
+
+        return result;
+    }
+}
