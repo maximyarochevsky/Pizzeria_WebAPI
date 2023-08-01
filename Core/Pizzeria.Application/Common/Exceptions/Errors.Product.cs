e904c63eb4b624025ecf756755cf7fb20d231687
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-01 19:34:18 +0300

    Ошибки для товаров

---
+++ Core/Pizzeria.Application/Common/Exceptions/Errors.Product.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Pizzeria.Application.Common.Exceptions;
+
+public static partial class Errors
+{
+    public static class Product
+    {
+        public static Error InvalidId => Error.Validation(
+            code: "Product.InvalidId",
+            description: "Id товара не найден.");
+
+        public static Error NotFound => Error.NotFound(
+            code: "Product.NotFound",
+            description: "Товар не найден.");
+    }
+}
