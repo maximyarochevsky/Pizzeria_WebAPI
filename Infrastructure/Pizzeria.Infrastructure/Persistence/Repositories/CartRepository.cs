ba305ef0b9c322a1c945a00d71ccf9d8da11c84e
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-01 19:32:06 +0300

    Инкремент товара в корзине

--- Infrastructure/Pizzeria.Infrastructure/Persistence/Repositories/CartRepository.cs
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/Repositories/CartRepository.cs
@@ -9,19 +9,19 @@ using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Pizzeria.Infrastructure.Persistence.Repositories;
 
-public class CartRepository : ICartRepository
+public class CartRepository :  ICartRepository
 {
     private readonly IServiceProvider _services;
     public CartRepository(IServiceProvider services)
     {
         _services = services;
     }
-
+    
     public bool AddCartItem(CartItem cartItem)
     {
         ISession _session = _services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
 
-        if (_session.GetString(cartItem.ProductId.ToString()) != cartItem.ProductId.ToString())
+        if(_session.GetString(cartItem.ProductId.ToString()) != cartItem.ProductId.ToString())
         {
             _session.SetString(cartItem.ProductId.ToString(), JsonSerializer.Serialize<CartItem>(cartItem));
             return true;
@@ -29,4 +29,24 @@ public class CartRepository : ICartRepository
         return false;
     }
 
+    public bool IncrementCartItem(Guid Id)
+    {
+        ISession _session = _services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+        if(_session.GetString(Id.ToString()) != null)
+        {
+            var item = JsonSerializer.Deserialize<CartItem>(_session.GetString(Id.ToString()));
+            decimal price = item.Price/item.Quantity;
+            item.Quantity++;
+            item.Price = item.Quantity * price; 
+            _session.SetString(Id.ToString(), JsonSerializer.Serialize<CartItem>(item));
+
+            return true;
+        }
+
+        return false;
+    }
+
+   
 }
+
