5e6b5cf52b4fcd9c7147c44f870bd1a938286419
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-01 19:31:26 +0300

    Добавление товара в корзину через сессию

--- Infrastructure/Pizzeria.Infrastructure/Persistence/Repositories/CartRepository.cs
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/Repositories/CartRepository.cs
@@ -5,22 +5,23 @@ using System.Net.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Pizzeria.Infrastructure.Persistence.Repositories;
 
-public class CartRepository :  ICartRepository
+public class CartRepository : ICartRepository
 {
     private readonly IServiceProvider _services;
     public CartRepository(IServiceProvider services)
     {
         _services = services;
     }
-    
+
     public bool AddCartItem(CartItem cartItem)
     {
         ISession _session = _services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
 
-        if(_session.GetString(cartItem.ProductId.ToString()) != cartItem.ProductId.ToString())
+        if (_session.GetString(cartItem.ProductId.ToString()) != cartItem.ProductId.ToString())
         {
             _session.SetString(cartItem.ProductId.ToString(), JsonSerializer.Serialize<CartItem>(cartItem));
             return true;
@@ -28,37 +29,4 @@ public class CartRepository :  ICartRepository
         return false;
     }
 
-    public bool ClearCart()
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool DecrementCartItem()
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<List<CartItem>> GetCart()
-    {
-        ISession _session = _services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
-        List<CartItem> list = new List<CartItem>();
-        foreach(var key in _session.Keys)
-        {
-            list.Add(JsonSerializer.Deserialize<CartItem>(_session.GetString(key)));
-        }
-
-        return list;
-    }
-
-    public bool IncrementCartItem()
-    {
-        throw new NotImplementedException();
-    }
-
-    public bool RemoveCartItem()
-    {
-        throw new NotImplementedException();
-    }
-
 }
-
