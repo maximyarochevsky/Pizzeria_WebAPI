bc1087e8cd991097f47f0001498e168b018e244e
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-02 12:48:25 +0300

    Стрококвые модели для секций

---
+++ Pizzeria.Contracts/Section/Get/GetSectionById.cs
@@ -0,0 +1,6 @@
+﻿namespace Pizzeria.Contracts.Section.Get;
+
+public class GetSectionById
+{
+    public string Name { get; set; }
+}
---
+++ Pizzeria.Contracts/Section/Get/GetSectionList.cs
@@ -0,0 +1,6 @@
+﻿namespace Pizzeria.Contracts.Section.Get;
+
+public class GetSectionList
+{
+    public List<GetSectionList> Sections { get; set; }
+}
