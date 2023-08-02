58e203f936c7230916cbfb26b5a4a10ff6f09784
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-02 12:46:48 +0300

    Ошибки для секций

---
+++ Core/Pizzeria.Application/Common/Exceptions/Errors.Section.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Application.Common.Exceptions;
+public static partial class Errors
+{
+    public static class Section
+    {
+        public static Error InvalidId => Error.Validation(
+            code: "Product.InvalidId",
+            description: "Id секции неверен.");
+
+        public static Error NotFound => Error.NotFound(
+            code: "Product.NotFound",
+            description: "Секция не найдена.");
+    }
+}
