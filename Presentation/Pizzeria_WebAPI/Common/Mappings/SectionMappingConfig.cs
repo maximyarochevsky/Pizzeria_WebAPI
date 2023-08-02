fad6a1d9f63b4d4ea9321aa7bc88a2a582241e65
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-02 12:49:01 +0300

    Конфигурация маппинга для секций в стрококвый вид

---
+++ Presentation/Pizzeria_WebAPI/Common/Mappings/SectionMappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Pizzeria.Application.Common.Mappings;
+using Pizzeria.Application.Sections.Queries.ViewModels;
+using Pizzeria.Contracts.Section.Get;
+
+namespace Pizzeria_WebAPI.Common.Mappings;
+
+public class SectionMappingConfig : IMapWith<SectionVm>
+{
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<SectionVm, GetSectionById>()
+            .ForMember(sectionVm => sectionVm.Name,
+                opt => opt.MapFrom(section => section.Name));
+
+      
+        profile.CreateMap<ListSectionsVm, GetSectionList>()
+            .ForMember(sectionsResponse => sectionsResponse.Sections,
+                opt => opt.MapFrom(sectionsVm => sectionsVm.ListSections));
+    }
+}
