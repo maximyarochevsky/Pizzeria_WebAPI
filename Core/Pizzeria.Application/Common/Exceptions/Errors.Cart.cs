56bf48781cd6131feafce68c5f32eb91baa27446
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-03 11:12:27 +0300

    Исключения для корзины

---
+++ Core/Pizzeria.Application/Common/Exceptions/Errors.Cart.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace Pizzeria.Application.Common.Exceptions;
+
+public static partial class Errors
+{
+    public static class Cart
+    {
+        public static Error NullCart => Error.NotFound(
+            code: "Cart.NullCart",
+            description: "Корзина содержит null.");
+    }
+}
