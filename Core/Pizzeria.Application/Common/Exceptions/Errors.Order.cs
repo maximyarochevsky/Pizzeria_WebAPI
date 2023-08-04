63f32bb042b789094b11b65b26702621133949df
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-04 15:51:40 +0300

    Исключения для заказов

---
+++ Core/Pizzeria.Application/Common/Exceptions/Errors.Order.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+
+namespace Pizzeria.Application.Common.Exceptions;
+
+public static partial class Errors
+{
+    public static class Order
+    {
+        public static Error InvalidId => Error.Validation(
+            code: "Product.InvalidId",
+            description: "Id заказа неверен.");
+
+        public static Error NotFound => Error.NotFound(
+            code: "Product.NotFound",
+            description: "Заказ не найден.");
+    }
+}
