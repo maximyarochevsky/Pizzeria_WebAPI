db512fd2e7f52f25f83f84361e537327d5a5e415
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-04 15:51:31 +0300

    Vm модель корзины

---
+++ Core/Pizzeria.Application/Cart/Queries/ViewModels/CartVm.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Pizzeria.Application.Common.Mappings;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Application.Cart.Queries.ViewModels;
+
+public class CartVm : IMapWith<Domain.Entities.Cart>
+{
+    public List<CartItemVm>? Items { get; set; }
+    public int ItemsCount => Items.Sum(item => item.Quantity);
+    public decimal TotalPrice { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Domain.Entities.Cart, CartVm>()
+               .ForMember(cartVm => cartVm.TotalPrice,
+                   opt => opt.MapFrom(cart => cart.TotalPrice))
+               .ForMember(cartVm => cartVm.ItemsCount,
+                   opt => opt.MapFrom(cart => cart.ItemsCount))
+               .ForMember(cartVm => cartVm.Items,
+                   opt => opt.MapFrom(cart => cart.Items));
+    }
+}
