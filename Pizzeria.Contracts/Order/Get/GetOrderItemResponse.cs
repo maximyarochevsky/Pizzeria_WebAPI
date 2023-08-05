7d41dfc4aaac6c1f52a85a8493c4d74536568959
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-05 22:45:03 +0300

    Стрококвая модель элемента заказа

---
+++ Pizzeria.Contracts/Order/Get/GetOrderItemResponse.cs
@@ -0,0 +1,12 @@
+﻿
+using Pizzeria.Contracts.Product.Get;
+
+namespace Pizzeria.Contracts.Order.Get
+{
+    public class GetOrderItemResponse
+    {
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public GetProductDetailsResponse Product { get; set; }
+    }
+}
