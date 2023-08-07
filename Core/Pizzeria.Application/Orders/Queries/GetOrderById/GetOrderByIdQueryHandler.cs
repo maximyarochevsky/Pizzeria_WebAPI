abce7bd8a24cb89ec765cb0e61db175873543eb8
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-07 16:07:54 +0300

    Обработчик запроса заказа по id

---
+++ Core/Pizzeria.Application/Orders/Queries/GetOrderById/GetOrderByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ErrorOr;
+using MediatR;
+using Pizzeria.Application.Common.Exceptions;
+using Pizzeria.Application.Interfaces.Persistence;
+using Pizzeria.Application.Orders.Queries.ViewModels;
+
+namespace Pizzeria.Application.Orders.Queries.GetOrderById;
+
+public class GetOrderByIdQueryHandler : IRequestHandler<GetOrderByIdQuery, ErrorOr<OrderVm>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    public GetOrderByIdQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    => (_unitOfWork, _mapper) = (unitOfWork, mapper);
+
+    public async Task<ErrorOr<OrderVm>> Handle(GetOrderByIdQuery request, CancellationToken cancellationToken)
+    {
+        if (!Guid.TryParse(request.Id.ToString(), out Guid result))
+        {
+            return Errors.Order.InvalidId;
+        }
+
+        var order = await _unitOfWork.Orders.GetOrderById(request.Id);
+
+        if (order == null)
+            return Errors.Order.NotFound;
+
+        return _mapper.Map<OrderVm>(order);
+    }
+}
