59b59030e86b9d4b979a4b94d677d8b5f54fc102
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-07 16:07:39 +0300

    Запрос заказа по id

---
+++ Core/Pizzeria.Application/Orders/Queries/GetOrderById/GetOrderByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+using Pizzeria.Application.Orders.Queries.ViewModels;
+
+namespace Pizzeria.Application.Orders.Queries.GetOrderById;
+
+public class GetOrderByIdQuery : IRequest<ErrorOr<OrderVm>>
+{
+    public Guid Id { get; set; }
+}
