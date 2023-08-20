e493834d411f62dacb4f739321ad91691f22a392
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-20 14:56:12 +0300

    Правила валидации для поиска продукта по id

---
+++ Core/Pizzeria.Application/Products/Queries/GetProductBySection/GetProductBySectionQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Pizzeria.Application.Products.Queries.GetProductBySection;
+
+public class GetProductBySectionQueryValidator : AbstractValidator<GetProductBySectionQuery>
+{
+    public GetProductBySectionQueryValidator()
+    {
+        RuleFor(x => x.SectionId).NotEqual(Guid.Empty);
+    }
+}
