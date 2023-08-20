9de7a665af643222b3914228d92706d4fb971559
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-20 14:57:01 +0300

    Правила валидации для согздания заказа

---
+++ Core/Pizzeria.Application/Orders/Commands/CreateOrder/CreateOrderCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Pizzeria.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(order => order.Address).MinimumLength(10);
+        RuleFor(order => order.Description).MaximumLength(400);
+        RuleFor(order => order.Phone).MaximumLength(13)
+        .MinimumLength(10)
+        .Matches(@"^\+?[0-9]+$").WithMessage("Номер телефона должен содержать только цифры и символ '+'."); 
+    }
+}
