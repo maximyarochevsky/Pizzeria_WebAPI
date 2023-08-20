e8b318167a036920af35e8ea79db9c00b8d5a164
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-20 14:59:16 +0300

    Vm модель заказа

---
+++ Pizzeria.Contracts/Order/Get/GetOrderResponse.cs
@@ -0,0 +1,15 @@
+﻿using Pizzeria.Application.Orders.Queries.ViewModels;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Contracts.Order.Get;
+
+public class GetOrderResponse
+{
+    public string TotalPrice { get; set; }
+    public string Description { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set; }
+    public string Date { get; set; }
+    public List<GetOrderItemResponse> OrderItems { get; set; }
+
+}
