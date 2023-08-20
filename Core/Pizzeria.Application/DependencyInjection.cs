5baea0ebbeed05c66d7c7b79156ac801b468fcbd
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-20 14:58:09 +0300

    Добавления сервиса валидации

--- Core/Pizzeria.Application/DependencyInjection.cs
+++ Core/Pizzeria.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Pizzeria.Application.Common.Behaviors;
 
 namespace Pizzeria.Application;
 
@@ -10,6 +12,12 @@ public static class DependencyInjection
         services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+
+        services.AddTransient(typeof(IPipelineBehavior<,>),
+            typeof(ValidationBehavior<,>));
+
         return services;
     }
 }
