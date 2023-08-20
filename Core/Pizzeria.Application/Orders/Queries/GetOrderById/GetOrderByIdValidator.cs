3b1a938c53d34f2ea0d0b447a442581233e305d3
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-20 14:57:17 +0300

    Правила валидации для поиска заказа по  id

---
+++ Core/Pizzeria.Application/Orders/Queries/GetOrderById/GetOrderByIdValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Pizzeria.Application.Orders.Queries.GetOrderById;
+
+public class GetOrderByIdValidator : AbstractValidator<GetOrderByIdQuery>
+{
+    public GetOrderByIdValidator()
+    {
+        RuleFor(x => x.Id).NotEqual(Guid.Empty);
+    }
+}
