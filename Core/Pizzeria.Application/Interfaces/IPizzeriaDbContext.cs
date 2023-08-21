788d88b288366bc64e44aa2fc32fe61ed3c875ed
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-21 18:41:39 +0300

    Добавление таблицы пользователей в БД контекст

--- Core/Pizzeria.Application/Interfaces/IPizzeriaDbContext.cs
+++ Core/Pizzeria.Application/Interfaces/IPizzeriaDbContext.cs
@@ -9,6 +9,7 @@ public interface IPizzeriaDbContext
    DbSet<Product> Products { get; set; }
    DbSet<OrderItem> OrderItems { get; set; }
    DbSet<Order> Orders { get; set; }
+   DbSet<User> Users { get; set; }
 
-   Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
