483a87aa6c733797af154b48370334e7aa83405b
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-21 18:43:54 +0300

    Запрос на вход в систему

---
+++ Core/Pizzeria.Application/Authentication/Queries/LoginQuery.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+using MediatR;
+using Pizzeria.Application.Authentication.Common;
+
+namespace Pizzeria.Application.Authentication.Queries;
+
+public class LoginQuery : IRequest<ErrorOr<AuthenticationResult>>
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
