8030be2335ca77da5ba5d6db01feaf9c60749663
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-21 18:41:52 +0300

    Конфигурация для таблицы пользователей

---
+++ Infrastructure/Pizzeria.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pizzeria.Domain.Entities;
+
+namespace Pizzeria.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(user => user.Id);
+        builder.Property(user => user.FirstName);
+        builder.Property(user => user.LastName);
+        builder.Property(user => user.Email);
+        builder.Property(user => user.Password).IsRequired();
+    }
+}
