6af1eb115f7446e666a122e9b1e9c01f304d4af0
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-21 18:43:44 +0300

    Правила для регистрации нового пользователя

---
+++ Core/Pizzeria.Application/Authentication/Commands/RegisterCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Pizzeria.Application.Orders.Commands.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizzeria.Application.Authentication.Commands
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(model => model.FirstName)
+             .NotEmpty().WithMessage("Имя не должно быть пустым.")
+             .MaximumLength(50).WithMessage("Имя не должно превышать 50 символов.");
+
+            RuleFor(model => model.LastName)
+                .NotEmpty().WithMessage("Фамилия не должна быть пустой.")
+                .MaximumLength(50).WithMessage("Фамилия не должна превышать 50 символов.");
+
+            RuleFor(model => model.Email)
+                .NotEmpty().WithMessage("Email не должен быть пустым.")
+                .EmailAddress().WithMessage("Некорректный формат Email.");
+
+            RuleFor(model => model.Password)
+                .NotEmpty().WithMessage("Пароль не должен быть пустым.")
+                .MinimumLength(6).WithMessage("Пароль должен содержать не менее 6 символов.");
+        }
+    }
+}
