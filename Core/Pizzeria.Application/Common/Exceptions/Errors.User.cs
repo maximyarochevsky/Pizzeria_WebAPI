e755c040dfde2c9314d35b6c01a4e35ba4ec4924
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-21 18:42:36 +0300

    Ошибки для регистрации и авторизации

---
+++ Core/Pizzeria.Application/Common/Exceptions/Errors.User.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+using System.Diagnostics;
+using System.Net;
+
+namespace Pizzeria.Application.Common.Exceptions;
+
+public static partial class Errors
+{
+    public static class User
+    {
+        public static Error DuplicateEmail => Error.Conflict(
+            code: "User.DuplicateEmail",
+            description: "Пользователь с данной почтой уже существует.");
+
+        public static Error InvalidPassword => Error.Validation(
+            code: "User.InvalidPassword",
+            description: "Неверный пароль.");
+
+        public static Error NotFound => Error.NotFound(
+            code: "User.NotFound",
+            description: "Пользователя с данной почтой не существует.");
+    }
+}
