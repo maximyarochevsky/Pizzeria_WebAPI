a179c3bb0cca7e942fef064758f62a23dff34f47
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-08-22 10:41:24 +0300

    Перенос сервисов для уровня Presentation в отдельную группу

---
+++ Presentation/Pizzeria_WebAPI/DependencyInjection.cs
@@ -0,0 +1,51 @@
+﻿using Pizzeria.Application.Interfaces;
+using Microsoft.OpenApi.Models;
+using Pizzeria.Application.Common.Mappings;
+using System.Reflection;
+
+namespace Pizzeria_WebAPI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection
+        services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(option =>
+        {
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+
+        services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(IPizzeriaDbContext).Assembly));
+        });
+
+        return services;
+    }
+}
